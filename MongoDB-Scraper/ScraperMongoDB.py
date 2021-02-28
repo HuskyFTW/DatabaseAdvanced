a9b0eedf21bf74b2d8afac569f74e111afb73be9
Diego Borghgraef <46396750+HuskyFTW@users.noreply.github.com>
2021-02-28 12:59:26 -0500

    Update ScraperMongoDB.py

--- MongoDB-Scraper/ScraperMongoDB.py
+++ MongoDB-Scraper/ScraperMongoDB.py
@@ -14,7 +14,6 @@ def scraperMongo():
     soup = BeautifulSoup(page.content, features="lxml")
     Elements = soup.find_all("span", class_="sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC")
     ElementsHash = soup.select("a.sc-1r996ns-0.fLwyDF.sc-1tbyx6t-1.kCGMTY.iklhnl-0.eEewhk.d53qjk-0.ctEFcK")    
-    content = []
     bitcoins = []
     usd = []
     for i in Elements:       
@@ -30,11 +29,9 @@ def scraperMongo():
         
     df = pd.DataFrame({"bitcoins": bitcoins, "dollars" : usd})   
     columns_BTC = df["bitcoins"]
-    columns_USD = df["dollars"]
     max_BTC = columns_BTC.max()   
     df_max = df[df["bitcoins"] == max_BTC]
     USD = df_max["dollars"].iloc[0]
-    print(USD)
     ElementsInfo = {"Bitcoins " : max_BTC, "Dollars" : USD}
     y = mycollection.insert_one(ElementsInfo)
     time.sleep(60) 
